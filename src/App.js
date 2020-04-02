ad008de6b8467e969c0ad5842fe2fe73c5575da6
Jorge Roldan <jorgeroldanroldan@gmail.com>
2020-04-02 11:57:56 -0400

    Done with lect9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
 class App extends React.Component{
 	render() {
 		return (
-			<div className="App">
-				<h1>Hello from React</h1>
+			<div className = 'App'>
+				<h1>Hello from React!</h1>
 			</div>
 		);
 	}
