cdd45f5973b0919ba2f959c5b18a4816340a5a39
Jorge Roldan <jorgeroldanroldan@gmail.com>
2020-04-02 13:44:50 -0400

    finished lect10

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
+import Navbar from "./components/layout/Navbar";
 
 class App extends React.Component{
 
 	render() {
-		const name = 'John Doe';
-		const loading = false;
-		const showName = false;
 
 		return (
 			<div className = 'App'>
-				{loading ? <h4>Loading....</h4> :	<h1>Hello {showName && name}</h1>}
+				<Navbar/>
 			</div>
 		);
 	}
