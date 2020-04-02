e7cf77610fddbb636a321b275844642f4bb930ab
Jorge Roldan <jorgeroldanroldan@gmail.com>
2020-04-02 14:18:56 -0400

    finished lect12

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 
-class App extends React.Component{
+class App extends Component{
 
 	render() {
 
 		return (
 			<div className = 'App'>
 				<Navbar/>
+				<UserItem />
 			</div>
 		);
 	}
